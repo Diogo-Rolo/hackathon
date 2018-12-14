3a483af54ad663ad34c83a209c7fee2fef96e6bb
RodQA <rodrigo.queiroga.amaral@gmail.com>
2018-12-14 01:06:29 +0000

    added controller stuff

---
+++ src/main/java/org/academiadecodigo/controller/CustomerController.java
@@ -0,0 +1,64 @@
+package org.academiadecodigo.controller;
+
+
+import org.academiadecodigo.dto.CustomerDto;
+import org.academiadecodigo.model.Customer;
+import org.academiadecodigo.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class CustomerController  {
+
+    private CustomerService customerService;
+    private List<Customer> customerList;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/addcustomer")
+    public String addCustomer() {
+
+        return "clientform";
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/addcustomer")
+    public String addCustomer(Customer customer) {
+        customerService.createCustomer(customer);
+        return "main";
+
+    }
+
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/login")
+    public String addNameAndPassword(String email, String password) {
+        customerList = customerService.getAllCustomers();
+
+        for (Customer customer : customerList){
+
+            if(email == customer.getEmail() && password == customer.getPassword()){
+
+                return "customer/"+customer.getId();
+
+            }
+
+        }
+
+        return "/login";
+
+    }
+
+
+
+
+}
